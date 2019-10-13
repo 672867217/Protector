443b5ab10f0fca85e7a621aca3e345a9a808afae
mtf0907 <1301470126@qq.com>
2019-10-13 13:58:03 +0800

    10/13 14:00 两个dailog

--- app/src/main/java/com/example/protector/MainActivity.java
+++ app/src/main/java/com/example/protector/MainActivity.java
@@ -2,9 +2,18 @@ package com.example.protector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
@@ -14,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button3;
     private Button button6;
     private Button button5;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +36,69 @@ public class MainActivity extends AppCompatActivity {
         button3 = (Button) findViewById(R.id.button3);
         button6 = (Button) findViewById(R.id.button6);
         button5 = (Button) findViewById(R.id.button5);
+
+
+//        editor = getSharedPreferences("diyici",MODE_PRIVATE).edit();
+//
+//        Timer timer = new Timer();
+//        TimerTask timerTask = new TimerTask() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        };
+//        timer.schedule(timerTask,0,5000);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = LayoutInflater.from(getApplication()).inflate(R.layout.dialog_test1,null);
+                final AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).setView(view).show();
+                Button dialog1_btn1 = view.findViewById(R.id.test_dialog1_btn1);
+                Button dialog1_btn2 = view.findViewById(R.id.test_dialog1_btn2);
+         /*
+                设置dialog 宽 高的大小
+                final WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width = 800;
+                params.height = 1200;
+                dialog.getWindow().setAttributes(params); */
+
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                dialog1_btn1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this,Test.class));
+                        dialog.dismiss();
+                    }
+                });
+                dialog1_btn2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        View view2 = LayoutInflater.from(getApplication()).inflate(R.layout.dialog_test2,null);
+                        final AlertDialog dialog2 = new AlertDialog.Builder(MainActivity.this).setView(view2).show();
+                        Button dialog2_btn1 = view2.findViewById(R.id.test_dialog2_btn1);
+                        Button dialog2_btn2 = view2.findViewById(R.id.test_dialog2_btn2);
+                        dialog2.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                        dialog2_btn1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(MainActivity.this,Test.class));
+                                dialog.dismiss();
+                                dialog2.dismiss();
+                            }
+                        });
+                        dialog2_btn2.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog2.dismiss();
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+
 
     }
 }
