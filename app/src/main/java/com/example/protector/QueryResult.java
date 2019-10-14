46e92b7bc822a8f49d2715af6d66d932290acece
mtf0907 <1301470126@qq.com>
2019-10-14 15:01:31 +0800

    10/15 15.00

---
+++ app/src/main/java/com/example/protector/QueryResult.java
@@ -0,0 +1,15 @@
+package com.example.protector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QueryResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_result);
+
+    }
+}
