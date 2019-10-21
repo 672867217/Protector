67c79785215f802fdb72fc0273102dbe3384ca73
mtf0907 <1301470126@qq.com>
2019-10-21 21:03:49 +0800

    10.21 21.00！

--- app/src/main/java/com/example/protector/Test.java
+++ app/src/main/java/com/example/protector/Test.java
@@ -2,37 +2,29 @@ package com.example.protector;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.protector.SQl.TestData;
+
+import org.litepal.crud.DataSupport;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
-import android.widget.Button;
-
-import com.example.protector.util.DataUtils;
 
 public class Test extends AppCompatActivity implements View.OnClickListener {
 
@@ -45,44 +37,106 @@ public class Test extends AppCompatActivity implements View.OnClickListener {
     private GridView test_gv2;
     TestGv1ItemAdapter testGv1ItemAdapter;
     TestGv2ItemAdapter testGv2ItemAdapter;
-    List list1 = new ArrayList();
-    List list2 = new ArrayList();
-
-    String[] gv1_name = {"一测统计","二测统计","三测统计","其他统计"};
-
+    private TextView stats_tv3;
+    private Spinner chanpin_spinner5;
+    private TextView stats_tv2;
+    private Spinner chanpin_spinner4;
+    private Spinner chanpin_spinner2;
+    private Spinner chanpin_spinner3;
+    private Spinner chanpin_spinner;
+    private TextView stats_tv1;
+    String[] sp_name = {"其他测程","一测", "二测", "三测"};
+    String[] gv1_name2 = {"一测", "二测", "三测","其他"};
+    List<Bean> list1 = new ArrayList();
+    List<TestData> list2 = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         initView();
-        String data = "AA00FF1049020100C804C4B8D20404008000E600F200F400FC00F1008E0CE90FF3F7080CF5371724094C0A3C0B18008800EC00F500E605190BD1040105E904DF03A203B103C3047E04E104220334";
 
-        List<String> dataList = new ArrayList<>();
-        for (int i = 0; i < data.length(); i++) {
-            dataList = new DataUtils().getDivLines(data,2);
-        }
-        for (int i = 0; i < dataList.size(); i++) {
-            System.out.println("这是第"+i+"条："+dataList.get(i));
-        }
-        for (int i = 0; i < 5; i++) {
-            if (i < 4) {
-                Bean1 bean1 = new Bean1();
-                bean1.name = gv1_name[i];
-                list1.add(bean1);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        stats_tv2.setText(dateFormat.format(new Date()));
+
+        final List<TestData> dataList = DataSupport.findAll(TestData.class);
+
+
+        for (int i = 1; i < 5; i++) {
+            int shuliang=0,zongshijian=0;
+            String num = String.valueOf(i);
+            if (i == 4) {
+                num = 0 + "";
             }
-
-            Bean1 bean2 = new Bean1();
-            bean2.name = "工位"+(i+1);
-            list2.add(bean2);
+            for (int j = 0; j < dataList.size(); j++) {
+                if (dataList.get(j).getCecheng().equals(num)) {
+                    shuliang++;
+                    zongshijian = Integer.parseInt(dataList.get(i).getCeshishichang());
+                }
+            }
+            Bean bean = new Bean();
+            bean.ceshi = String.valueOf(shuliang);
+            bean.tongguo = String.valueOf(shuliang);
+            bean.weitongguo = String.valueOf(shuliang);
+            bean.yongshi = zongshijian/60+"'"+zongshijian%60+"s";
+            list1.add(bean);
         }
-        testGv1ItemAdapter = new TestGv1ItemAdapter(this,list1);
+
+        testGv1ItemAdapter = new TestGv1ItemAdapter(this, list1);
         test_gv1.setAdapter(testGv1ItemAdapter);
         testGv1ItemAdapter.notifyDataSetChanged();
 
-        testGv2ItemAdapter = new TestGv2ItemAdapter(this,list2);
+        testGv2ItemAdapter = new TestGv2ItemAdapter(this, list2);
         test_gv2.setAdapter(testGv2ItemAdapter);
         testGv2ItemAdapter.notifyDataSetChanged();
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(Test.this, android.R.layout.simple_spinner_dropdown_item, sp_name);
+        chanpin_spinner3.setAdapter(arrayAdapter);
+        chanpin_spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                list2.clear();
+                switch (position) {
+                    case 0:
+                        for (int i = 0; i < 5; i++) {
+                            if (dataList.get(i).getCecheng().equals("0")) {
+                                list2.add(dataList.get(i));
+                            }
+                        }
+                        break;
+                    case 1:
+                        for (int i = 0; i < 5; i++) {
+                            if (dataList.get(i).getCecheng().equals("1")) {
+                                list2.add(dataList.get(i));
+                            }
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < 5; i++) {
+                            if (dataList.get(i).getCecheng().equals("2")) {
+                                list2.add(dataList.get(i));
+                            }
+                        }
+                        break;
+                    case 3:
+                        for (int i = 0; i < 5; i++) {
+                            if (dataList.get(i).getCecheng().equals("3")) {
+                                list2.add(dataList.get(i));
+                            }
+                        }
+                        break;
+                }
+
+                //选完以后更新适配器
+                testGv2ItemAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
     }
@@ -95,7 +149,15 @@ public class Test extends AppCompatActivity implements View.OnClickListener {
         test_btn = (Button) findViewById(R.id.test_btn);
         test_gv1 = (GridView) findViewById(R.id.test_gv1);
         test_gv2 = (GridView) findViewById(R.id.test_gv2);
-
+        stats_tv3 = (TextView) findViewById(R.id.stats_tv3);
+        stats_tv2 = (TextView) findViewById(R.id.stats_tv2);
+        stats_tv1 = (TextView) findViewById(R.id.stats_tv1);
+        chanpin_spinner5 = (Spinner) findViewById(R.id.chanpin_spinner5);
+        chanpin_spinner4 = (Spinner) findViewById(R.id.chanpin_spinner4);
+        chanpin_spinner2 = (Spinner) findViewById(R.id.chanpin_spinner2);
+        chanpin_spinner3 = (Spinner) findViewById(R.id.chanpin_spinner3);
+        chanpin_spinner = (Spinner) findViewById(R.id.chanpin_spinner);
+        stats_tv1 = (TextView) findViewById(R.id.stats_tv1);
         test_btn.setOnClickListener(this);
     }
 
@@ -108,19 +170,17 @@ public class Test extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
-    class Bean1{
-        String name;
-        int value1,value2,value3,value4;
+    class Bean{
+        String ceshi,tongguo,weitongguo,yongshi;
     }
-
     public class TestGv1ItemAdapter extends BaseAdapter {
 
-        private List<Bean1> objects = new ArrayList<Bean1>();
+        private List<Bean> objects = new ArrayList<Bean>();
 
         private Context context;
         private LayoutInflater layoutInflater;
 
-        public TestGv1ItemAdapter(Context context,List<Bean1> objects) {
+        public TestGv1ItemAdapter(Context context, List<Bean> objects) {
             this.context = context;
             this.objects = objects;
             this.layoutInflater = LayoutInflater.from(context);
@@ -132,7 +192,7 @@ public class Test extends AppCompatActivity implements View.OnClickListener {
         }
 
         @Override
-        public Bean1 getItem(int position) {
+        public Bean getItem(int position) {
             return objects.get(position);
         }
 
@@ -147,17 +207,17 @@ public class Test extends AppCompatActivity implements View.OnClickListener {
                 convertView = layoutInflater.inflate(R.layout.test_gv1_item, null);
                 convertView.setTag(new ViewHolder(convertView));
             }
-            initializeViews(getItem(position), (ViewHolder) convertView.getTag());
+            initializeViews(getItem(position), (ViewHolder) convertView.getTag(),position);
             return convertView;
         }
 
-        private void initializeViews(Bean1 bean1, ViewHolder holder) {
+        private void initializeViews(Bean bean1, ViewHolder holder,int what) {
             //TODO implement
-            holder.testItem1Tv.setText(bean1.name);
-            holder.testItem1Tv1.setText(bean1.value1+"");
-            holder.testItem1Tv2.setText(bean1.value2+"");
-            holder.testItem1Tv3.setText(bean1.value3+"");
-            holder.testItem1Tv4.setText(bean1.value4+"");
+            holder.testItem1Tv.setText(gv1_name2[what]+"统计");
+            holder.testItem1Tv1.setText(bean1.ceshi);
+            holder.testItem1Tv2.setText(bean1.tongguo);
+            holder.testItem1Tv3.setText(bean1.weitongguo);
+            holder.testItem1Tv4.setText(bean1.yongshi);
         }
 
         protected class ViewHolder {
@@ -177,17 +237,14 @@ public class Test extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
-
-//   class Bean2{}
-
     public class TestGv2ItemAdapter extends BaseAdapter {
 
-        private List<Bean1> objects = new ArrayList<Bean1>();
+        private List<TestData> objects = new ArrayList<TestData>();
 
         private Context context;
         private LayoutInflater layoutInflater;
 
-        public TestGv2ItemAdapter(Context context,List<Bean1> objects) {
+        public TestGv2ItemAdapter(Context context, List<TestData> objects) {
             this.context = context;
             this.objects = objects;
             this.layoutInflater = LayoutInflater.from(context);
@@ -199,7 +256,7 @@ public class Test extends AppCompatActivity implements View.OnClickListener {
         }
 
         @Override
-        public Bean1 getItem(int position) {
+        public TestData getItem(int position) {
             return objects.get(position);
         }
 
@@ -214,22 +271,41 @@ public class Test extends AppCompatActivity implements View.OnClickListener {
                 convertView = layoutInflater.inflate(R.layout.test_gv2_item, null);
                 convertView.setTag(new ViewHolder(convertView));
             }
-            initializeViews(getItem(position), (ViewHolder) convertView.getTag());
+            initializeViews(getItem(position), (ViewHolder) convertView.getTag(),position);
             return convertView;
         }
 
-        private void initializeViews(Bean1 bean1, ViewHolder holder) {
+        private void initializeViews(TestData testData, ViewHolder holder,int what) {
             //TODO implement
-            holder.testItem2Tv.setText(bean1.name);
-            holder.testItem2Tv1.setText(bean1.value1+"");
-            holder.testItem2Tv2.setText(bean1.value2+"");
-            holder.testItem2Tv3.setText(bean1.value3+"");
-            holder.testItem2Tv4.setText(bean1.value4+"");
+            holder.testItem2Tv.setText("工位"+(what+1));
+            holder.testItem2Tv1.setText("自动");
+            holder.testItem2Tv2.setText(testData.getChanpinbianma());
+            holder.testItem2Tv3.setText(testData.getChanpinbianma());
+            holder.testItem2Tv4.setText(testData.getCecheng() );
+            if (Integer.parseInt(testData.getCeshishichang()) < 60) {
+                holder.testItem2Tv5.setText(testData.getCeshishichang()+"s");
+            } else {
+
+                holder.testItem2Tv5.setText(Integer.parseInt(testData.getCeshishichang())/60+"'"
+                        +Integer.parseInt(testData.getCeshishichang())%60+"s");
+            }
+            //测试结果
+            holder.testItem2Tv7.setText(testData.getQidongshijian());
+            holder.testItem2Tv8.setText(testData.getQidongshijian());
+            holder.testItem2Tv9.setText(testData.getM13xianshishijian());
+            holder.testItem2Tv10.setText(testData.getM30xianshishijian());
+            holder.testItem2Tv11.setText(testData.getQidongshijian());
+            holder.testItem2Tv12.setText(testData.getQidongshijian());
+            holder.testItem2Tv13.setText(testData.getQidongshijian());
+            holder.testItem2Tv14.setText(testData.getQidongshijian());
+            holder.testItem2Tv15.setText(testData.getQidongshijian());
+            holder.testItem2Tv16.setText("无");
+            holder.testItem2Tv17.setText("合格");
 
             holder.testBtn2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    View view = LayoutInflater.from(Test.this).inflate(R.layout.dialog_test4,null);
+                    View view = LayoutInflater.from(Test.this).inflate(R.layout.dialog_test4, null);
                     final Dialog dialog = new AlertDialog.Builder(Test.this).setView(view).show();
                     dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 //                    final Timer timer = new Timer();
